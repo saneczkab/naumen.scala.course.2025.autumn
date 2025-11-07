6e35eb86c4f09a6fd128ceff389a0890691d22ae
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-11-07 17:37:38 +0500

    Task 1 done

--- homeworks/homework_5/src/main/scala/Task1.scala
+++ homeworks/homework_5/src/main/scala/Task1.scala
@@ -45,9 +45,18 @@ object Task1 extends App {
   }
 
   object ShowInstance {
-    implicit val catShow: Show[Cat] = ???
+    implicit val catShow: Show[Cat] = {
+      case VeryLittleCat(name) => s"очень маленький кот $name"
+      case LittleCat(name) => s"маленький кот $name"
+      case NormalCat(name) => s"кот $name"
+      case BigCat(name) => s"большой кот $name"
+      case VeryBigCat(name) => s"очень большой кот $name"
+    }
 
-    implicit def boxShow[A: Show]: Show[Box[A]] = ???
+    implicit def boxShow[A: Show]: Show[Box[A]] = {
+      case EmptyBox => "пустая коробка"
+      case BoxWith(value) => s"${implicitly[Show[A]].show(value)} в коробке"
+    }
   }
 
   object ShowSyntax {
