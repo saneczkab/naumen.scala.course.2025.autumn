93b7afdd1acb112c414f25ccee5842186c8ab1a2
Alexandr Iarmoshenko <a2398253@gmail.com>
2025-12-04 20:34:27 +0500

    ResuourceTraining task done

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -13,9 +13,25 @@ import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+  def readData(filePath: String): IO[Throwable, String] = {
+    ZIO.acquireReleaseWith {
+      ZIO.succeed(new BufferedReader(new FileReader(filePath)))
+    } {
+      reader => ZIO.succeed(reader.close())
+    } {
+      reader => ZIO.succeed(reader.readLine())
+    }
+  }
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = {
+    ZIO.acquireReleaseWith {
+      ZIO.succeed(new BufferedWriter(new FileWriter(filePath)))
+    } {
+        writer => ZIO.succeed(writer.close())
+    } {
+        writer => ZIO.succeed(writer.write(data))
+    }
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
